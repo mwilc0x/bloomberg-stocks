68161b2d9d3adb7df9452f2f759e7b783c29f1c0
Mike Wilcox <mwilcox56@gmail.com>
2014-01-05 20:13:53 -0500

    add mongodb

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var socketio = require('socket.io'),
     http = require('http'),
     express = require('express'),
     app = express(),
-    port = process.env.PORT || 3000;
+    port = process.env.PORT || 3000,
+    MongoClient = require('mongodb').MongoClient,
+    format = require('util').format;
 
 app.use(express.static(__dirname + '/dist'));
 
